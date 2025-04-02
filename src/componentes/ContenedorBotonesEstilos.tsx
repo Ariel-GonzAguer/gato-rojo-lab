4f1a07b4968b40ace1343b277989ed863f25e419
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-04-01 19:21:49 -0600

    se agregan tipografías y se avanza con estilos 🚀

--- src/componentes/ContenedorBotonesEstilos.tsx
+++ src/componentes/ContenedorBotonesEstilos.tsx
@@ -5,19 +5,31 @@ import useStore from "../store/useStore";
 import { Diseño } from "../types/types";
 
 export default function ContenedorBotonesEstilos() {
-  const { diseñoInStore, cambiarDiseño } = useStore();
+  const { cambiarDiseño } = useStore();
+
+  // handle cambio de estilos
+  function handleDiseño(estilo: Diseño) {
+    const body = document.body;
+    body.classList.add("blur");
+
+    cambiarDiseño(estilo);
+
+    setTimeout(() => {
+      body.classList.remove("blur");
+    }, 900);
+  }
 
   return (
     <section className="contenedorEstilos">
-      <button onClick={() => cambiarDiseño(Diseño.dark)}>Dark</button>
-      <button onClick={() => cambiarDiseño(Diseño.light)}>Light</button>
-      <button onClick={() => cambiarDiseño(Diseño.neumorphism)}>
+      <button onClick={() => handleDiseño(Diseño.dark)}>Dark</button>
+      <button onClick={() => handleDiseño(Diseño.light)}>Light</button>
+      <button onClick={() => handleDiseño(Diseño.neumorphism)}>
         Neumorphism
       </button>
-      <button onClick={() => cambiarDiseño(Diseño.neubrutalism)}>
+      <button onClick={() => handleDiseño(Diseño.neubrutalism)}>
         Neubrutalism
       </button>
-      <button onClick={() => cambiarDiseño(Diseño.gamer)}>Gamer</button>
+      <button onClick={() => handleDiseño(Diseño.gamer)}>Gamer</button>
     </section>
   );
 }
---
+++ src/layouts/ContenedorEstilos.tsx
@@ -0,0 +1,10 @@
+import useStore from "../store/useStore";
+
+export default function ContenedorEstilos({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { diseñoInStore } = useStore();
+  return <div className={diseñoInStore}>{children}</div>;
+}
