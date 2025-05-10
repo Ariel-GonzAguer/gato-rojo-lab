cb543c9da32962dee77ace29f693316058d78beb
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-05-09 19:44:19 -0600

    feat(tests): enhance useStore tests with Spanish descriptions and additional state update validation

--- tests/store.test.tsx
+++ tests/store.test.tsx
@@ -1,9 +1,23 @@
 import { describe, it, expect } from 'vitest';
 import useStore from '../src/store/useStore';
+import { act } from 'react';
+import { Diseño } from '../src/types/types';
 
 describe('useStore', () => {
-  it('should initialize with default values', () => {
+  it('debe inicializarse correctamente', () => {
     const store = useStore.getState();
     expect(store).toBeDefined();
+  });
+
+  it('debe actualizar el estado correctamente', () => {
+    const estadoInicial = useStore.getState();
+    expect(estadoInicial.diseñoInStore).toBe(Diseño.dark);
+
+    act(() => {
+      useStore.setState({ diseñoInStore: Diseño.gamer });
+    });
+
+    const estadoActual = useStore.getState();
+    expect(estadoActual).not.toBe(estadoInicial);
   });
 });
